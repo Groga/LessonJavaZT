be4a0432d5bacb88f82ecb5d00462a980e3e74b6
groga <grog65@gmail.com>
2018-10-21 14:11:04 +0300

    первый комит из идеи01

--- src/Gui01/Gui01.java
+++ src/Gui01/Gui01.java
@@ -1,6 +1,18 @@
+import  javax.swing.JOptionPane;
+
 package Gui01;
 
 public class Gui01 {
+    int i, k;
+    String n1, n2;
+
+    public void Scan(){
+
+        n1=JOptionPane.showInputDialog("Ведите первое число : ");
+        n2=JOptionPane.showInputDialog("Ведите второе число : ");
+
+
+    }
 
 
 
